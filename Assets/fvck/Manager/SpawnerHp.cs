581956b0b2a77901f240ab622c59d2e7e0837c03
rafinothere <rafiul00751@gmail.com>
2024-07-21 18:55:53 +0100

    asd
    asd

---
+++ Assets/fvck/Manager/SpawnerHp.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnerHp : MonoBehaviour
+{
+    public Image healthBar;
+    public float healthAmount = 100f;
+
+    void Update()
+    {
+        if (healthAmount <= 0)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            TakeDamage(20);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Heal(5);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+        {
+        if (collision.gameObject.CompareTag("Enemy"))
+            {
+                TakeDamage(20); 
+            }
+        }
+
+    public void TakeDamage(float damage)
+    {
+        healthAmount -= damage;
+        healthBar.fillAmount = healthAmount / 100f;
+    }
+
+    public void Heal(float healingAmount)
+    {
+        healthAmount += healingAmount;
+        healthAmount = Mathf.Clamp(healthAmount, 0, 100);
+        healthBar.fillAmount = healthAmount / 100f;
+    }
+}
