bd1967d4ba634bc8641540807321ac9d3f339edd
ym-vs-games <ymiah3498@gmail.com>
2024-07-25 17:15:55 +0100

    asda

--- Assets/yhya/scripts/healthPickup.cs
+++ Assets/yhya/scripts/healthPickup.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Health : MonoBehaviour
+public class healthPickup : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D Heal)
     {
