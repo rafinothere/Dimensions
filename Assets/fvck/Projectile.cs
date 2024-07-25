c9919fab30c1ec2ea5c7c69243e2e4297062d1b4
rafinothere <rafiul00751@gmail.com>
2024-07-25 16:36:45 +0100

    asd
    asd

--- Assets/fvck/Projectile.cs
+++ Assets/fvck/Projectile.cs
@@ -5,43 +5,32 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public float spawnOffset = 0.5f;
-    private int spawnedEnemyCount = 0; // Counter for spawned enemies
+    private bool enemySpawned = false; // Flag to check if an enemy has been spawned
 
-    public List<GameObject> enemyPrefabs;  // List of enemy prefabs
+    public GameObject enemyPrefab;  // Single enemy prefab
 
     private float elapsedTime = 0f;  // Time elapsed since projectile creation
 
     void Update()
     {
         elapsedTime += Time.deltaTime;
-        if (elapsedTime >= 0.5f && spawnedEnemyCount < 2)
+        if (elapsedTime >= 0.5f && !enemySpawned)
         {
-            StartCoroutine(SpawnEnemies());
+            StartCoroutine(SpawnEnemy());
         }
     }
 
-    private IEnumerator SpawnEnemies()
+    private IEnumerator SpawnEnemy()
     {
         yield return new WaitForSeconds(0.5f);
 
-        if (spawnedEnemyCount < 2 && enemyPrefabs.Count >= 2)
+        if (!enemySpawned)
         {
-            // Randomly select two enemy prefabs from the list
-            int index1 = Random.Range(0, enemyPrefabs.Count);
-            int index2;
-            do
-            {
-                index2 = Random.Range(0, enemyPrefabs.Count);
-            } while (index2 == index1);
+            // Spawn the enemy prefab at the projectile's position
+            Instantiate(enemyPrefab, transform.position, transform.rotation);
 
-            Vector3 spawnPosition1 = transform.position + new Vector3(-spawnOffset, 0, 0);
-            Vector3 spawnPosition2 = transform.position + new Vector3(spawnOffset, 0, 0);
-
-            // Instantiate the selected enemy prefabs at the calculated positions
-            Instantiate(enemyPrefabs[index1], spawnPosition1, transform.rotation);
-            Instantiate(enemyPrefabs[index2], spawnPosition2, transform.rotation);
-
-            spawnedEnemyCount += 2; // Increment the spawned enemy count by 2
+            enemySpawned = true; // Mark that the enemy has been spawned
         }
     }
 }
+
