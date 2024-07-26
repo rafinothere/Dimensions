c2bf56ad205480ce0aa61b59035bf3cf7a21e02b
rafinothere <rafiul00751@gmail.com>
2024-07-26 16:25:34 +0100

    asd
    asd

--- Assets/fvck/Manager/BehaviourManager.cs
+++ Assets/fvck/Manager/BehaviourManager.cs
@@ -1,18 +1,57 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BehaviourManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<MonoBehaviour> FollowBehaviors;
+    public List<MonoBehaviour> RangeBehaviors;
+    public List<MonoBehaviour> HordeBehaviors;
+    public List<MonoBehaviour> HealthManagerBehaviors;
+
+    // Call this method to activate all behaviors
+    public void ActivateBehaviors()
     {
-        
+        StartCoroutine(ActivateAllBehaviors());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator ActivateAllBehaviors()
     {
-        
+        foreach (var behavior in FollowBehaviors)
+        {
+            if (behavior is IBehavior)
+            {
+                ((IBehavior)behavior).Activate();
+                yield return new WaitForSeconds(1); // wait for 1 second before the next activation
+            }
+        }
+
+        foreach (var behavior in RangeBehaviors)
+        {
+            if (behavior is IBehavior)
+            {
+                ((IBehavior)behavior).Activate();
+                yield return new WaitForSeconds(1);
+            }
+        }
+
+        foreach (var behavior in HordeBehaviors)
+        {
+            if (behavior is IBehavior)
+            {
+                ((IBehavior)behavior).Activate();
+                yield return new WaitForSeconds(1);
+            }
+        }
+
+        foreach (var behavior in HealthManagerBehaviors)
+        {
+            if (behavior is IBehavior)
+            {
+                ((IBehavior)behavior).Activate();
+                yield return new WaitForSeconds(1);
+            }
+        }
     }
 }
+*/
