45e6b799346a6534b308f66ceed0eeb5cd75653d
ym-vs-games <ymiah3498@gmail.com>
2024-07-27 21:13:27 +0100

    qq

--- Assets/yhya/scripts/enemy behaviour/sniper enemy.cs
+++ Assets/yhya/scripts/enemy behaviour/sniper enemy.cs
@@ -4,5 +4,39 @@ using UnityEngine;
 
 public class sniperenemy : MonoBehaviour
 {
-    
+    private enemyrandom random;
+    private enemyrun run;
+    private pauseshot shoot;
+
+    void Start()
+    {
+        random = GetComponent<enemyrandom>();
+        run = GetComponent<enemyrun>();
+        shoot = GetComponent<pauseshot>();
+    }
+
+    void Update()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if(Vector2.Distance(transform.position,player.transform.position) > 50f)
+        {
+            random.enabled = true;
+            run.enabled = false;
+            shoot.enabled = false;
+        }
+        else if ((Vector2.Distance(transform.position,player.transform.position) < 50f) && (Vector2.Distance(transform.position,player.transform.position) > 5f))
+        {
+            random.enabled = false;
+            run.enabled = false;
+            shoot.enabled = true;
+        }
+        else
+        {
+            random.enabled = false;
+            run.enabled = true;
+            shoot.enabled = false;
+        }
+
+    }
+
 }
